e15de56b93090496e71d77d841c2977b6a6917b1
Yugam18 <er.yugam@gmail.com>
2025-08-14 11:22:18 +0530

    changes

--- kidscamp-store/src/app/api/product/[id]/route.ts
+++ kidscamp-store/src/app/api/product/[id]/route.ts
@@ -6,8 +6,8 @@ export async function GET(
     { params }: { params: Promise<{ id: string }> }
 ) {
     const { id } = await params;
-
-    const product = productsData.products.find((p) => p.id === id || p.category_id === id);
+    console.log('id',id)
+    const product = productsData.products.find((p) => p.id === id);
 
     if (!product) {
         return NextResponse.json(
